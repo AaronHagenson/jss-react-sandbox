6d05c8784c276c2966c5bac64d559269d5d60baf
Aaron Hagenson <aaron.hagenson@legrand.com>
2022-04-25 10:53:46 -0500

    create css file

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import { Text } from '@sitecore-jss/sitecore-jss-react';
 
+import styles from './login.module.scss';
+
 const Login = (props) => (
   <div>
-    <p>Login Component</p>
+    <p className={styles.testThis}>Login Component</p>
     <Text field={props.fields.heading} />
   </div>
 );
